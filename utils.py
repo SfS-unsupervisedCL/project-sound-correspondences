2dd46606ad2e4876cbe65a0eab407ba8647bb938
Verena Blaschke <verena.blaschke@gmx.de>
2018-01-18 23:37:57 +0100

    simple speech sound representation

---
+++ utils.py
@@ -0,0 +1,55 @@
+from preprocessing.phon_inventory import process_line
+from phone import Phone
+
+ipa_symbols = dict()
+
+def read_ipa(ipa_file):
+	ipa_symbols = dict()
+	with open(ipa_file, 'r', encoding='utf-8') as f:
+		for line in f:
+			line = line.strip()
+			if line.startswith('#') or len(line) == 0:
+				continue
+			#symbol,type,manner,place,voice,secondary,vertical,horizontal,rounding,length,nasalization
+			# TODO make this dynamic instead?
+			fields = line.split(',')
+			assert len(fields) == 11
+			symbol = fields[0]
+			phone = Phone(sound_type=fields[1], 
+				manner=fields[2], place=fields[3], 
+				voice=fields[4], secondary=fields[5], 
+				vertical=fields[6], horizontal=fields[7], 
+				rounding=fields[8], length=fields[9], 
+				nasalization=fields[10])
+			ipa_symbols[symbol] = phone
+	return ipa_symbols
+
+def to_phone(symbol):
+	if len(symbol) == 1:
+		return ipa_symbols[symbol]
+	phone = ipa_symbols[symbol[0]]
+	if '͡' in symbol:
+		phone.manner = 'affricate'
+	if 'ʲ' in symbol:
+		phone.secondary = 'palatalized'
+	if 'ː' in symbol:
+		phone.length = 'long'
+	elif 'ˑ' in symbol:
+		phone.length = 'half long'
+	return phone
+
+if __name__ == "__main__":
+	ipa_symbols = read_ipa("preprocessing/ipa.csv")
+	word = 'tʲɪt͡ʃʲeˑnʲijə'
+	word_symbols = process_line(word)
+	# ['tʲ', 'ɪ', 't͡ʃʲ', 'eˑ', 'nʲ', 'i', 'j', 'ə']
+	word_phones = []
+	for symbol in word_symbols:
+		word_phones.append(to_phone(symbol))
+	print(word)
+	print(word_symbols)
+	print(word_phones)	
+	print(word_symbols[0], word_symbols[1], word_phones[0].distance(word_phones[1]))
+	print(word_symbols[0], word_symbols[4], word_phones[0].distance(word_phones[4]))
+	print(word_symbols[1], word_symbols[3], word_phones[1].distance(word_phones[3]))
+	print(word_symbols[1], word_symbols[7], word_phones[1].distance(word_phones[7]))
