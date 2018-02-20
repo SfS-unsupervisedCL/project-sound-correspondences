3e4c214cf64655282c4f09c2c4facc3c7fd3f1a5
Verena Blaschke <verena.blaschke@gmx.de>
2018-02-20 15:57:48 +0100

    use correct labels for graphviz; add sample decision tree predicting the manner of consonant artic.

--- tree/tree.py
+++ tree/tree.py
@@ -1,10 +1,27 @@
 from sklearn import tree
 import graphviz
 import numpy as np
+import re
+import sys
+
+def actual_class_names(labels, class_names_all):
+	"""
+	Returns a list containing the names of the classes
+	that actually appear in the data.
+	The resulting list can be used as the class_names argument of
+	sklearn.tree.export_graphviz.
+
+	Keyword arguments:
+	labels -- the column that the tree should predict
+	class_names_all -- the list of class names, as given in transform_ipa
+	"""
+	unique = np.unique(labels).tolist()
+	return [class_names_all[i] for i in unique]
 
 
-def build_tree(in_file):
+def build_tree(in_file, out_dir):
 	# predict deu_itself_manner
+	feature = "deu_itself_manner"
 	with open(in_file, 'r', encoding='utf-8') as f:
 		header = f.readlines()[0].split(',')
 	n_cols = len(header)
@@ -13,33 +30,33 @@ def build_tree(in_file):
 
 	data_cols = list(range(n_cols))
 	data_cols.remove(label_col)
-	# print("label", label_col)
-	# print("data", data_cols)
-	labels = np.loadtxt(open(in_file, 'r'), delimiter=",", skiprows=1, usecols=label_col)
-	data = np.loadtxt(open(in_file, 'r', encoding='utf-8'), delimiter=",", skiprows=1, usecols=data_cols)
+	labels = np.loadtxt(open(in_file, 'r'), delimiter=",", dtype=np.int32, skiprows=1, usecols=label_col)
+	data = np.loadtxt(open(in_file, 'r'), delimiter=",", dtype=np.int32, skiprows=1, usecols=data_cols)
 
-	# print(data.shape)
-	# print(labels.shape)
-	# print(data[:10])
-	# print(labels[:1])
+	# TODO import this list
+	class_names = actual_class_names(labels, ['', 'plosive', 'tap', 'trill', 'affricate', 'fricative',
+                 'lateral fricative', 'lateral approximant', 'approximant',
+                 'nasal'])
 
 	clf = tree.DecisionTreeClassifier(criterion='entropy')
 	clf = clf.fit(data, labels)
 
 	dot_data = tree.export_graphviz(clf, out_file=None, 
 		feature_names=header, 
-		# TODO import this
-		class_names=['', 'plosive', 'tap', 'trill', 'affricate', 'fricative',
-          'lateral fricative', 'lateral approximant', 'approximant',
-          'nasal'],
+		class_names=class_names,
 		filled=True, # colour + hue by class + certainty
 		rounded=True, # purely cosmetic
 		special_characters=True # less-than-or-equals sign
 		)
-	graph = graphviz.Source(dot_data) 
-	graph.render("deu_manner") 
+
+	graph = graphviz.Source(dot_data)
+	outfile = out_dir + '/' + re.sub('itself_', '', feature)
+	graph.render(outfile) 
 
 
 if __name__ == "__main__":
-	in_file = "../data/deu-swe-features.csv"
-	build_tree(in_file)
+    if len(sys.argv) < 3:
+        sys.stderr.write('Usage: %s FEATURES OUTPUT_DIR\n' % sys.argv[0])
+        sys.exit(1)
+
+    build_tree(sys.argv[1], sys.argv[2])
